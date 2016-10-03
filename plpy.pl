84402ddb9fc6db916825e310f4887040f9cd4deb
neilbaksi <snb.1996@gmail.com>
2016-10-03 20:36:17 +1100

    added more code for test cases

--- plpy.pl
+++ plpy.pl
@@ -151,25 +151,28 @@ sub arithmeticLines {
 	# $#
 	$_[0] =~ s/\$\#ARGV/len\(sys\.argv\)/;
 
+    if($_[0] =~ /(\w+)\s*([><=!%]+)\s*(\d+)\s*/){ # case for when STDIN is a number
+        $_[0] =~ s/($1)/float\($1\)/;
+    }
+    if($_[0] =~ /float\((\w+)\)\s*(=)\s*(\d+)\s*/){# case to not match basic initialisation
+        $_[0] =~ s/float\(($1)\)/$1/;    
+    }
 	# removes $,@, my
 	$_[0] =~ s/\$//g;
 	$_[0] =~ s/\@//g;
 	$_[0] =~ s/my//;
 
 	# STDIN
-	if($line =~ /(\$[^\s]*)\s*%|[<>=]+\s*[\d]+/){ # case for when STDIN is a number
-		$_[0] =~ s/\<STDIN\>/float\(sys\.stdin\.readline\(\)\)/;	
-	}
-	else {
-        if($_[0] =~ /<STDIN>/ && $flag == 0){
+	if($_[0] =~ /<STDIN>/ && $flag == 0){
 		    &printWhiteSpaces($whiteSpaces);
 		    print "import sys\n";
 		    $flag = 1;
-		  }
-		$_[0] =~ s/\<STDIN\>/sys\.stdin\.readline\(\)/;
-		
-		}
-
+		    $_[0] =~ s/\<STDIN\>/sys\.stdin\.readline\(\)/;
+		    }
+    
+	  
+	#$string =~ s/(\$\w+)\s*([><=!]+)\s*(\d+)\s*(?=:)/float($1) $2 $3/;
+	       
 	# and/or/not
 	$_[0] =~ s/\&\&/and /g;
 	$_[0] =~ s/\|\|/or /g;
@@ -216,7 +219,6 @@ sub arithmeticLines {
 # to deal with chomp
 	} elsif ($line =~ /^\s*chomp\s*\$(.*)\s*;$/) {
 		&printWhiteSpaces($whiteSpaces - $loopCounter);
-	#} elsif ($line =~ /^\s*chomp\s+\$(.*);/){
 		$variable = $1;
 		$line =~ s/[^\s].*//g;
 		chomp $line;
@@ -257,7 +259,8 @@ sub arithmeticLines {
 		# to deal with the specific case of range where end is $#ARGV
 		if($line =~ /0\.\.\$#ARGV/){
 		        $line =~ s/0\.\.\$#ARGV/range(len(sys.argv)-1)/g;
-		}
+		        $flag = 2;
+ 		}
 		$line =~ s/each*|\$*//g;
 		$line =~ s/\(/in /;
 		$line =~ s/\)\s*{/:/g;
@@ -270,6 +273,9 @@ sub arithmeticLines {
 		$line =~ s/ARGV/sys.argv[1:]/g;
 			
 		$whiteSpaces ++;
+		if($flag == 2){
+		    print "import sys\n";
+		}
 		print "$line";
 
 
@@ -317,9 +323,9 @@ sub arithmeticLines {
 		$whiteSpaces --;
 
 # to deal with array conversion
-	}elsif ($line =~ /^\s*(.*)\s*\@(.*)\s*(.*)\s*;$/) { #array in the line	
+	}elsif ($line =~ /^\s*(.*)\s*\@(.*)\s*(.*)\s*;$/) { # array in the line	
 		my $arrayName = $2; 
-		if (/^\s*(.*)\s*\@(.*)\s*=\s*((.*))\s*;$/) { #declaring the array
+		if (/^\s*(.*)\s*\@(.*)\s*=\s*((.*))\s*;$/) { # declaring the array
 			$line =~ s/\@//;
 			$line =~ s/my//;
 			$line =~ s/\(/\[/;
@@ -327,7 +333,7 @@ sub arithmeticLines {
 			$line =~ s/\"/\'/g;
 			$line =~ s/\;//;
 			print "$line";
-		} else { #accessing the array
+		} else { # accessing the array
 			$line =~ s/\@//;
 			$line =~ s/my//;
 			$line =~ s/\;//;
@@ -348,19 +354,29 @@ sub arithmeticLines {
 		&printWhiteSpaces($whiteSpaces);
 		print "$1.unshift($2)\n";
 
-# to deal with pop
+# to deal with shift
 	} elsif ($line =~ /^\s*shift\s*\@(.*);$/) {
 		&printWhiteSpaces($whiteSpaces);
 		print "$1.shift\n";
 
 # to translate 's///' command
-	} elsif ($line =~ /\$(.*)\s*=~\s*s\/(.*)\/(.*)\/g*/){
+	} elsif ($line =~ /(.*)\s*=~\s*s\/(.*)\/(.*)\/g*;/){
 	        my $variable = $1;
 	        my $phrase = $2;
 	        my $replace = $3;
-	  	$line =~ s/[\$;]//g;
-	  	$line =~ s/~\s*s\/.*\/.*\/g*/ re.sub(r'$phrase', '$replace', $variable)/g;
-	    	print $line;
+	  	    $line =~ s/[\$;]//g;
+	  	    #$line =~ s/~\s*s\/.*\/.*\/g*/ re.sub(r'$phrase', '$replace', $variable)/g;
+	    	print "$variable = re.sub(r'$phrase', '$replace', $variable)";
+	    	$whiteSpaces ++;
+	    	print "++++++++++++++";
+# to deal with hash tables
+	} elsif ($line =~ /^\s*\$[^\s]*{(.*)}\s*=\s*(.*);/){
+
+		$line =~ s/\$//g;
+		$line =~ s/{/[/g;
+		$line =~ s/}/]/g;
+		print $line;
+    
 # Could not translate -> comments
 	} else { 
 		print "#$line\n";
